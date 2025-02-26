0cb700a8cc1f722541484567baaaae7bf9300412
ZherKing <49807821+ZherKing@users.noreply.github.com>
2025-02-26 19:17:48 +0800

    更新多线程 软件跑更快

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import subprocess
 import sys
 import os
 import ctypes
+import threading
 
 class ServiceManager:
     def __init__(self, log_text_widget):
@@ -30,54 +31,64 @@ class ServiceManager:
             self.log(f"执行命令失败: {command}\n{e.stderr}")
             messagebox.showerror("错误", f"执行命令失败: {command}")
 
+    def run_command_threaded(self, command):
+        thread = threading.Thread(target=self.run_command, args=(command,))
+        thread.start()
+
     # FTP部分
     def enable_ftp_feature(self):
         self.log("正在启用IIS和FTP服务功能...")
-        self.run_command("dism /online /enable-feature /featurename:IIS-WebServerRole /all")
-        self.run_command("dism /online /enable-feature /featurename:IIS-FTPServer /all")
+        self.run_command_threaded("dism /online /enable-feature /featurename:IIS-WebServerRole /all")
+        self.run_command_threaded("dism /online /enable-feature /featurename:IIS-FTPServer /all")
         self.log("IIS和FTP服务功能已启用")
+
+    def disable_ftp_feature(self):
+        self.log("正在禁用IIS和FTP服务功能...")
+        self.run_command_threaded("dism /online /disable-feature /featurename:IIS-WebServerRole")
+        self.run_command_threaded("dism /online /disable-feature /featurename:IIS-FTPServer")
+        self.log("IIS和FTP服务功能已禁用")
 
     def start_ftp(self):
         self.log("正在启动FTP服务器...")
-        self.run_command("net start ftpsvc")
+        self.run_command_threaded("net start ftpsvc")
         self.log("FTP服务器已启动")
         messagebox.showinfo("信息", "FTP服务器已启动")
 
     def stop_ftp(self):
         self.log("正在停止FTP服务器...")
-        self.run_command("net stop ftpsvc")
+        self.run_command_threaded("net stop ftpsvc")
         self.log("FTP服务器已停止")
         messagebox.showinfo("信息", "FTP服务器已停止")
 
     def start_iis(self):
         self.log("正在启动IIS...")
-        self.run_command("start inetmgr")
+        self.run_command_threaded("start inetmgr")
         self.log("IIS已启动")
         messagebox.showinfo("信息", "IIS已启动")
 
     # SMB部分
     def start_smb(self):
         self.log("正在启用 SMB 服务...")
-        self.run_command("dism /online /enable-feature /featurename:SMB1Protocol /all")
+        self.run_command_threaded("dism /online /enable-feature /featurename:SMB1Protocol /all")
         self.log("SMB 服务已启动")
         messagebox.showinfo("信息", "SMB服务已经启用（可能需要重启）")
 
     def stop_smb(self):
         self.log("正在停止SMB服务器...")
-        self.run_command("dism /online /disable-feature /featurename:SMB1Protocol")
+        self.run_command_threaded("dism /online /disable-feature /featurename:SMB1Protocol")
         self.log("SMB服务器已停止")
         messagebox.showinfo("信息", "SMB服务器已停止")
 
     # NFS部分
     def start_nfs(self):
         self.log("正在启用 NFS 服务...")
-        self.run_command("dism /online /enable-feature /featurename:ServicesForNFS-Server /all")
+        self.run_command_threaded("dism /online /enable-feature /featurename:ServicesForNFS-Server /all")
         self.log("NFS 服务已启用")
         messagebox.showinfo("信息", "NFS 服务已启用")
 
     def stop_nfs(self):
         self.log("正在停止 NFS 服务...")
-        self.run_command("dism /online /disable-feature /featurename:ServicesForNFS-Server")
+        self.run_command_threaded("dism /online /disable-feature /featurename:ServicesForNFS-Server")
         self.log("NFS 服务器已停止")
         messagebox.showinfo("信息", "NFS 服务已停止")
 
@@ -107,6 +118,9 @@ class App:
 
         self.enable_ftp_feature_button = tk.Button(root, text="启用FTP服务", command=self.manager.enable_ftp_feature)
         self.enable_ftp_feature_button.pack(pady=5)
+
+        self.disable_ftp_feature_button = tk.Button(root, text="禁用FTP服务", command=self.manager.disable_ftp_feature)
+        self.disable_ftp_feature_button.pack(pady=5)
 
         self.start_iis_button = tk.Button(root, text="启动 IIS", command=self.manager.start_iis)
         self.start_iis_button.pack(pady=5)
